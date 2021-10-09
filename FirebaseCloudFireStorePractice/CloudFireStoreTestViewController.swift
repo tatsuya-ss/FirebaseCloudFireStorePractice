c895f6d7350056ce1c335a09f9ff7bb973e3d8ee
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-09 21:55:52 +0900

    testで使用する画面作成

---
+++ FirebaseCloudFireStorePractice/CloudFireStoreTestViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CloudFireStoreTestViewController.swift
+//  FirebaseCloudFireStorePractice
+//
+//  Created by 坂本龍哉 on 2021/10/09.
+//
+
+import UIKit
+
+final class CloudFireStoreTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
