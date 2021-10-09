adf3adbd9c95bf823dfee68fe31e1a05aead9f2e
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-09 21:33:18 +0900

    画面作成

---
+++ FirebaseCloudFireStorePractice/FirebaseUtil.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseUtil.swift
+//  FirebaseCloudFireStorePractice
+//
+//  Created by 坂本龍哉 on 2021/10/09.
+//
+
+import Foundation
--- FirebaseCloudFireStorePractice/LoginViewController.swift
+++ FirebaseCloudFireStorePractice/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  FirebaseCloudFireStorePractice
 //
 //  Created by 坂本龍哉 on 2021/10/09.
@@ -7,19 +7,18 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class LoginViewController: UIViewController {
 
+    @IBOutlet private weak var mainAddressTextField: UITextField!
+    @IBOutlet private weak var passWordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    }
-
-    @IBAction private func signUpButtonDidTap(_ sender: Any) {
-        
     }
     
     @IBAction private func loginButtonDidTap(_ sender: Any) {
         
     }
+    
 }
-
