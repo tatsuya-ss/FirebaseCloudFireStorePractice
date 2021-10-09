3c120129dfa5338401f810173ae48f339edfe862
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-09 21:34:49 +0900

    Firebase初期化とサインアップ処理作成

--- FirebaseCloudFireStorePractice/FirebaseUtil.swift
+++ FirebaseCloudFireStorePractice/FirebaseUtil.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import Firebase
+
+final class FirebaseUtil {
+    
+    func signUp(email: String,
+                password: String,
+                completion: @escaping (Result<Void, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email,
+                               password: password) { authResult, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            completion(.success(()))
+        }
+    }
+    
+}
