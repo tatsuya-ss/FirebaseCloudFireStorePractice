87c4c01f40e08349067f43cf9b85952499043aac
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-09 21:51:26 +0900

    SignUp画面作成

---
+++ FirebaseCloudFireStorePractice/SignUpViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SignUpViewController.swift
+//  FirebaseCloudFireStorePractice
+//
+//  Created by 坂本龍哉 on 2021/10/09.
+//
+
+import UIKit
+
+final class SignUpViewController: UIViewController {
+
+    @IBOutlet private weak var mailAddressTextField: UITextField!
+    @IBOutlet private weak var passWordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction private func signUpButtonDidTap(_ sender: Any) {
+        guard let mail = mailAddressTextField.text,
+              let password = passWordTextField.text else { return }
+        FirebaseUtil().signUp(email: mail,
+                              password: password) { result in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success():
+                print("成功")
+            }
+        }
+    }
+    
+}
