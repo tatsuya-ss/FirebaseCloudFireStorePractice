ee0fc47893ec89ca6dd18b37efb5fd1d00df17ad
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-14 15:03:12 +0900

    firestoreとstorageの保存を非同期で行う処理

--- FirebaseCloudFireStorePractice/CloudFireStoreTestViewController.swift
+++ FirebaseCloudFireStorePractice/CloudFireStoreTestViewController.swift
@@ -8,20 +8,26 @@
 import UIKit
 
 final class CloudFireStoreTestViewController: UIViewController {
-
+    
     @IBOutlet private weak var postTextField: UITextField!
     @IBOutlet private weak var postIdTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     @IBAction private func postDocumentIdButtonDidTap(_ sender: Any) {
-        guard let post = postTextField.text else { return }
-        let firebaseUtil = FirebaseUtil()
+        guard let post = postTextField.text,
+              let data = UIImage(named: "pasta")?.pngData() else { return }
         let postId = UUID().uuidString
-        firebaseUtil.save(post: post, postId: postId) { result in
+        
+        let dispatchGroup = DispatchGroup()
+        dispatchGroup.enter()
+        dispatchGroup.enter()
+        
+        FirebaseUtil().save(post: post, postId: postId) { result in
+            defer { dispatchGroup.leave() }
             switch result {
             case .success:
                 print("保存完了")
@@ -29,10 +35,7 @@ final class CloudFireStoreTestViewController: UIViewController {
                 print(error)
             }
         }
-        
-        let image = UIImage(named: "pasta")
-        guard let data = image?.pngData() else { return }
-        firebaseUtil.saveStorage(postId: postId, data: data) { result in
+        FirebaseUtil().saveStorage(postId: postId, data: data) { result in
             switch result {
             case .success(.resume):
                 print("resume")
@@ -42,11 +45,16 @@ final class CloudFireStoreTestViewController: UIViewController {
                 print(String(percent))
             case .success(.success):
                 print("写真の保存できました。")
+                dispatchGroup.leave()
             case .failure(let error):
                 print(error)
             }
         }
+        dispatchGroup.notify(queue: .main) {
+            print("全ての保存完了")
+        }
     }
+    
 }
 
 extension CloudFireStoreTestViewController {
